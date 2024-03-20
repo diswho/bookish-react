cdb49a8aca41bdf17bc9c7e75f3136bbf2bf04d2
diswho <discover.who@gmail.com>
2024-03-20 17:42:06 +0700

    finish

--- src/BookDetail/BookDetail.tsx
+++ src/BookDetail/BookDetail.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Book } from "../types";
 
+const getDescriptionFor = (book: Book) => {
+  return book.description ? book.description : book.name;
+};
 const BookDetail = ({ book }: { book: Book }) => {
   return (
     <div className="detail">
       <h2 className="book-title">{book.name}</h2>
-      <p className="book-description">{book.description}</p>
+      <p className="book-description">{getDescriptionFor(book)}</p>
     </div>
   );
 };
